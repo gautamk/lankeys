39cc9f6cd625c7ec5b0b6c47d5ec3b60e8c3dc1a
Gautam K <gautam.nitheesh@gmail.com>
2015-02-02 05:25:04 +0530

    Websocket Server running

---
+++ app/src/main/java/com/gautamk/lankeys/MainActivity.java
@@ -0,0 +1,105 @@
+package com.gautamk.lankeys;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.koushikdutta.async.AsyncServer;
+import com.koushikdutta.async.callback.CompletedCallback;
+import com.koushikdutta.async.http.WebSocket;
+import com.koushikdutta.async.http.server.AsyncHttpServer;
+import com.koushikdutta.async.http.server.AsyncHttpServerRequest;
+import com.koushikdutta.async.http.server.AsyncHttpServerResponse;
+import com.koushikdutta.async.http.server.HttpServerRequestCallback;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends ActionBarActivity {
+
+
+    private AsyncHttpServer server;
+    private ArrayList<WebSocket> sockets;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        this.server = new AsyncHttpServer();
+        server.websocket("/live", new AsyncHttpServer.WebSocketRequestCallback() {
+            @Override
+            public void onConnected(final WebSocket webSocket, AsyncHttpServerRequest request) {
+                //Use this to clean up any references to your websocket
+                webSocket.setClosedCallback(new CompletedCallback() {
+                    @Override
+                    public void onCompleted(Exception ex) {
+                        try {
+                            if (ex != null)
+                                Log.e("WebSocket", "Error");
+                        } finally {
+
+                        }
+                    }
+                });
+
+                webSocket.setStringCallback(new WebSocket.StringCallback() {
+                    @Override
+                    public void onStringAvailable(String s) {
+                        toast(s);
+                    }
+                });
+
+            }
+        });
+        server.listen(8888);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        server.stop();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void toast(final String message) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+                Log.d("SeverMessage", message);
+            }
+        });
+
+    }
+
+}
